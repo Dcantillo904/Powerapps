919a7e2f750dea4f975cc4de239fe278da7267e0
Daniela <113536895+Dcantillo904@users.noreply.github.com>
2024-03-15 15:43:33 -0400

    Update Script-02GF200.js

--- 02GF200 Edit/Script-02GF200.js
+++ 02GF200 Edit/Script-02GF200.js
@@ -1,4 +1,27 @@
 // Handles loading the events for <model-viewer>'s slotted progress bar
+const modelViewer = document.getElementById("viewer");
+
+async function downloadPosterToBlob() {
+    const blob = await modelViewer.toBlob({ idealAspect: false });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "modelViewer_toBlob.png";
+    a.click();
+    URL.revokeObjectURL(url);
+}
+
+function downloadPosterToDataURL() {
+    const url = modelViewer.toDataURL();
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "modelViewer_toDataURL.png";
+    a.click();
+    URL.revokeObjectURL(url);
+}
+document.querySelector("#download-button").addEventListener("click", downloadPosterToBlob);
+
+
 const onProgress = (event) => {
   const progressBar = event.target.querySelector('.progress-bar');
   const updatingBar = event.target.querySelector('.update-bar');
